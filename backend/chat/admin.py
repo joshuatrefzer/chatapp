a180bd0490848cc1220dd85696ef785d8b984008
joshuatrefzer <121818554+joshuatrefzer@users.noreply.github.com>
2024-03-27 18:25:13 +0100

    admin

--- backend/chat/admin.py
+++ backend/chat/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Channel, Message, Thread
 
 # Register your models here.
+class ChannelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'is_channel')
+    filter_horizontal = ('members', 'read_by')
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('author', 'content', 'created_at', 'source')
+    filter_horizontal = ('reactions',)
+
+class ThreadAdmin(admin.ModelAdmin):
+    list_display = ('author', 'content', 'created_at', 'source')
+    filter_horizontal = ('reactions',)
+
+admin.site.register(Channel, ChannelAdmin)
+admin.site.register(Message, MessageAdmin)
+admin.site.register(Thread, ThreadAdmin)
