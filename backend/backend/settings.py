b4775eee85e032733e331609bb924d41752aee41
joshuatrefzer <121818554+joshuatrefzer@users.noreply.github.com>
2024-03-31 11:38:30 +0200

    static

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -151,7 +151,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT =   "/var/www/html/" #os.path.join(BASE_DIR, "static")
+STATIC_ROOT =   os.path.join(BASE_DIR, "static") #"/var/www/html/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
