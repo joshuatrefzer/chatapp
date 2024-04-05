3c6a821a5b944404b515833ce7e4e7399890037b
joshuatrefzer <121818554+joshuatrefzer@users.noreply.github.com>
2024-04-05 12:26:07 +0200

    file upload

--- backend/chat/views.py
+++ backend/chat/views.py
@@ -22,7 +22,19 @@ class ChannelViewSet(viewsets.ModelViewSet):
     
     def destroy(self, request, *args, **kwargs):
         return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
- 
+    
+    # def create(self, request, *args, **kwargs):
+    #     serializer = self.get_serializer(data=request.data)
+    #     serializer.is_valid(raise_exception=True)
+    #     self.perform_create(serializer)
+    #     headers = self.get_success_headers(serializer.data)
+    #     return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+    
+    def perform_create(self, serializer):
+        if 'picture' in self.request.FILES:
+            serializer.validated_data['picture'] = self.request.FILES['picture']
+        serializer.save()
+    
     
 class ThreadViewset(viewsets.ModelViewSet):
     queryset = Thread.objects.all()
